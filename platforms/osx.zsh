75a0b155178552151c6f4a878d4c45a049e49337
Gabriel Evans <gabriel@codeconcoction.com>
2012-02-10 15:47:58 -0800

    Clean up .zshrc and OSX customizations

--- platforms/osx.zsh
+++ platforms/osx.zsh
@@ -1,5 +1,5 @@
 # Add osx plugin
-plugins+=(osx)
+plugins+=(osx brew)
 
 # Set node modules path
 export NODE_PATH="/usr/local/lib/node_modules"
@@ -16,23 +16,22 @@ if [[ $TERM_PROGRAM == "Apple_Terminal" ]] && [[ -z "$INSIDE_EMACS" ]] {
   chpwd
 }
 
-# Load solarized directory colors
-if [[ `uname` == "Darwin" ]] {
-  # Use GNU ls and dircolors
-  function ls() { gls --color=auto "$@"; }
-  eval `gdircolors $DOTFILES/dircolors-solarized/dircolors.ansi-dark`
+# Use GNU ls and dircolors
+function ls() { gls --color=auto "$@"; }
 
-  # All commands have been installed with the prefix 'g'.
-  #
-  # A file that aliases these commands to their normal names is available
-  # and may be used in your bashrc like:
-  #
-  #source /usr/local/Cellar/coreutils/8.12/aliases
-  #
-  # But note that sourcing these aliases will cause them to be used instead
-  # of Bash built-in commands, which may cause problems in shell scripts.
-  # The Bash "printf" built-in behaves differently than gprintf, for instance,
-  # which is known to cause problems with "bash-completion".
-  #
-  # The man pages are still referenced with the g-prefix.
-}
+# Load solarized directory colors
+eval `gdircolors $DOTFILES/dircolors-solarized/dircolors.ansi-dark`
+
+# All commands have been installed with the prefix 'g'.
+#
+# A file that aliases these commands to their normal names is available
+# and may be used in your bashrc like:
+#
+#source /usr/local/Cellar/coreutils/8.12/aliases
+#
+# But note that sourcing these aliases will cause them to be used instead
+# of Bash built-in commands, which may cause problems in shell scripts.
+# The Bash "printf" built-in behaves differently than gprintf, for instance,
+# which is known to cause problems with "bash-completion".
+#
+# The man pages are still referenced with the g-prefix.
