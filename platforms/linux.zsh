cce018265dfc4be8f8a8f05106c8a378cfae9448
Gabriel Evans <gabriel@codeconcoction.com>
2012-08-06 13:25:35 -0700

    Use rbenv on Linux

--- platforms/linux.zsh
+++ platforms/linux.zsh
@@ -1,22 +1,12 @@
-# Add osx plugin
-plugins+=(osx)
-
 # Set node modules path
-export NODE_PATH="/usr/local/lib/node_modules"
-
-# Set Apple Terminal.app resume directory
-if [[ $TERM_PROGRAM == "Apple_Terminal" ]] && [[ -z "$INSIDE_EMACS" ]] {
-  function chpwd {
-    local SEARCH=' '
-    local REPLACE='%20'
-    local PWD_URL="file://$HOSTNAME${PWD//$SEARCH/$REPLACE}"
-    printf '\e]7;%s\a' $PWD_URL
-  }
-
-  chpwd
-}
+#export NODE_PATH="/usr/local/lib/node_modules"
 
 alias ls="ls --color=auto"
+
+# Add rbenv to path
+PATH="$HOME/.rbenv/bin:$PATH"
+
+source "$HOME/.rbenv/completions/rbenv.zsh"
 
 # Load solarized directory colors
 eval `dircolors $DOTFILES/dircolors-solarized/dircolors.ansi-dark`
