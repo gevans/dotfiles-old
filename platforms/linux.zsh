c41ba7344746a971dc6ca94bdbf951beb00645b8
Gabriel Evans <gabriel@codeconcoction.com>
2012-11-22 01:13:05 -0800

    Update linux-specific zshrc

--- platforms/linux.zsh
+++ platforms/linux.zsh
@@ -1,12 +1,14 @@
-# Set node modules path
-#export NODE_PATH="/usr/local/lib/node_modules"
-
 alias ls="ls --color=auto"
 alias tmux="TERM=screen-256color-bce tmux"
 
-# Add rbenv to path
-PATH="$HOME/.rbenv/bin:$PATH"
+# Set TERM to fix vim in tmux
+TERM="screen-256color"
 
+# Add Pyrus (PHP libraries) executables to path
+PATH="$HOME/pyrus/bin:$PATH"
+
+# Add rbenv to path and source completions
+PATH="$HOME/.rbenv/bin:$PATH"
 source "$HOME/.rbenv/completions/rbenv.zsh"
 
 # Load solarized directory colors
