39fde6fcbf623eae08a617b201873ab5eb467862
Gabriel Evans <gabriel@codeconcoction.com>
2013-03-12 13:53:42 -0700

    Add knife plugin to zshrc

--- zshrc.zsh
+++ zshrc.zsh
@@ -52,7 +52,7 @@ fpath=(/usr/local/share/zsh-completions $fpath)
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git bundler gem git-flow heroku vagrant projects eifion)
+plugins=(git bundler gem git-flow heroku vagrant projects eifion knife)
 
 PLATFORM=`uname`
 
